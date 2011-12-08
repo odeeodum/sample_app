fe83f5ae4ce96c9d12c6e59336bab7980fd79eba
Adam Haymond <coastgrd@gmail.com>
2011-12-07 20:02:01 -0700

    Completed all of Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-  # Return a tilte ona per-page basis
+  
+  # Return logo for layout page
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
+  # Return a tilte on a per-page basis
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
